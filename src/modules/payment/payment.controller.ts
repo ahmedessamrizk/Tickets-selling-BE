101eaaef0104578e114fcd46cb3153d1af209466
ahmedessamrizk <ahmed.essam7722@gmail.com>
2024-09-07 18:36:47 +0300

    push changes

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -49,8 +49,6 @@ export class PaymentController {
   }
 
   @Get('/most-sold')
-  @Roles(Role.User, Role.Admin, Role.SuperAdmin)
-  @UseGuards(RolesGuard)
   getMostSoldTickets(@Query() query: any): Promise<any> {
     const quantity = query.quantity ? Number(query.quantity) : 5;
     return this.paymentService.getTicketBought(-1, quantity, {
